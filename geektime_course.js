08a32703abfe3fe11ad322e8ac113f2909f3ac42
will <maoguangxian@famulei.com>
2019-03-03 13:54:48 +0800

    check video size when download

--- geektime_course.js
+++ geektime_course.js
@@ -8,6 +8,10 @@ const child_process = require('child_process');
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(pluginStealth());
 
+const CHROME_PATH = 'C:\\chrome-win32\\chrome.exe';
+const GOHLS_PATH = 'C:\\gohls\\gohls.exe -l=true ';
+const VIDEO_ERROR_SIZE = 1024*1024;
+
 (async () => {
     LOGIN_NAME = process.env.LOGIN_NAME
     PASSWORD = process.env.PASSWORD
@@ -18,7 +22,7 @@ puppeteer.use(pluginStealth());
     console.log(LOGIN_NAME, PASSWORD, COURSE_ID)
     options = {
         // headless: false,
-        executablePath: 'C:\\chrome-win32\\chrome.exe'
+        executablePath: CHROME_PATH
     }
     const browser = await puppeteer.launch(options)
     const page = await browser.newPage()
@@ -30,7 +34,7 @@ puppeteer.use(pluginStealth());
         'https://account.geekbang.org/signin?redirect=https%3A%2F%2Ftime.geekbang.org%2f%3fcategory%3d3'
     )
     const warning_dialog_confirm_button = await page.$(
-        'body > div.confirm-box-wrapper > div.confirm-box > div.foot > a'
+        'body > div:nth-child(12) > div.confirm-box > div.foot > a'
     )
     if (warning_dialog_confirm_button != null) {
         warning_dialog_confirm_button.click()
@@ -86,7 +90,7 @@ puppeteer.use(pluginStealth());
     await page.waitForNavigation()
     await page.waitForSelector('#app > div.page-home > div.content > ul > li:nth-child(1)')
 
-    // console.log(columns)
+
     for (let i = 0; i < columns.length; i++) {
         column = columns_dict[columns[i]]
         const url = 'https://time.geekbang.org/course/intro/' + column.id
@@ -94,14 +98,14 @@ puppeteer.use(pluginStealth());
         const resp = await page.waitForResponse(
             'https://time.geekbang.org/serv/v1/column/articles'
         )
-        // console.log(resp)
+
         const post_data = JSON.parse(resp.request().postData())
         const data = await resp.json()
         const cid = post_data.cid
         columns_articles_dict[cid] = data.data.list
     }
 
-    // console.log(columns_articles_dict)
+
     for (let i = 0; i < columns.length; i++) {
         column_title = columns_dict[columns[i]].column_title
         column_title = column_title.replace(/[/\\\?%*:\|"<>\.& ]/g, '')
@@ -121,29 +125,33 @@ puppeteer.use(pluginStealth());
 
         video_url = columns_dict[columns[i]].column_video_url
         video_file_path = path.join(column_path, '00---课程介绍' + '.mp4')
-        const cmd =
-            'C:\\gohls\\gohls.exe -l=true ' + video_url + ' ' + video_file_path
+        const cmd = GOHLS_PATH + video_url + ' ' + video_file_path
         console.log(cmd)
-        child_process.execSync(cmd)
+        child_process.execSync(cmd, { stdio: 'ignore' });
 
-        // console.log(articles)
         for (let i = 0; i < articles.length; i++) {
             title = articles[i].article_title
             title = title.replace(/[/\\\?%*:\|"<>\.& ]/g, '-')
             console.log(title)
-            try {
-                video_url = articles[i].video_media_map.hd.url
-                video_file_path = path.join(column_path, title + '.mp4')
-                const cmd =
-                    'C:\\gohls\\gohls.exe -l=true ' + video_url + ' ' + video_file_path
-                console.log(cmd)
-                child_process.execSync(cmd)
-            } catch (error) {
-                console.log('no audio')
+            const max_retry = 3
+            for (let n = 0; n < max_retry; n++) {
+                try {
+                    video_url = articles[i].video_media_map.hd.url
+                    video_size = articles[i].video_media_map.hd.size
+                    video_file_path = path.join(column_path, title + '.mp4')
+                    const cmd = GOHLS_PATH + video_url + ' ' + video_file_path
+                    console.log(cmd)
+                    child_process.execSync(cmd, { stdio: 'ignore' })
+                    const file_size = fs.statSync(video_file_path).size
+                    const delta = Math.abs(file_size - video_size)
+                    if (delta < VIDEO_ERROR_SIZE) {
+                        break
+                    }
+                } catch (error) {
+                    console.log('no video');
+                }
             }
         }
-        // break
     }
-
     await browser.close()
 })()
